01784a234d10a25196e130aa640702ad8873f6c1
Husnoro <Husnoro>
2020-03-29 23:43:51 +0500

    with fourth ex

--- Program.cs
+++ Program.cs
@@ -6,100 +6,101 @@ namespace homeWork2803
     {
         static void Main(string[] args)
         {
-            // // {// ex #2.1
-            // //     Console.WriteLine("Enter number: ");
-            // //     int firstNumber = int.Parse(Console.ReadLine());
-            // //     Console.WriteLine("Enter another number: ");
-            // //     int secondNumber = int.Parse(Console.ReadLine());
-            // //     int summa = 0;
-            // //     for (int i= firstNumber; i <= secondNumber; i++){
-            // //             summa += i;
+            {// ex #2.1
+                Console.WriteLine("Enter number: ");
+                int firstNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter another number: ");
+                int secondNumber = int.Parse(Console.ReadLine());
+                int summa = 0;
+                for (int i= firstNumber; i <= secondNumber; i++){
+                        summa += i;
                         
-            // //         }
-            // //         Console.WriteLine(summa);
+                    }
+                    Console.WriteLine(summa);
             
-            // // }
-            // // {// ex #2.2
-            // //     Console.WriteLine("Enter number: ");
-            // //     int firstNumber = int.Parse(Console.ReadLine());
-            // //     Console.WriteLine("Enter another number: ");
-            // //     int secondNumber = int.Parse(Console.ReadLine());
-            // //     for (int i = 1 + firstNumber; i < secondNumber; i++){
-            // //          if (i % 2 == 1){ 
-            // //         Console.WriteLine(i);
-            // //         }
+            }
+            {// ex #2.2
+                Console.WriteLine("Enter number: ");
+                int firstNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter another number: ");
+                int secondNumber = int.Parse(Console.ReadLine());
+                for (int i = 1 + firstNumber; i < secondNumber; i++){
+                     if (i % 2 == 1){ 
+                    Console.WriteLine(i);
+                    }
 
-            // //     }
+                }
 
-            // // }
-            // {// ex #3.1
-            //     Console.WriteLine("Enter rectangle length: ");
-            //     int rectangleLength = int.Parse(Console.ReadLine());
-            //     Console.WriteLine("Enter rectangle width: ");
-            //     int rectangleWidth = int.Parse(Console.ReadLine());
-            //     string length = "";
+            }
+            {// ex #3.1
+                Console.WriteLine("Enter rectangle length: ");
+                int rectangleLength = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter rectangle width: ");
+                int rectangleWidth = int.Parse(Console.ReadLine());
+                string length = "";
 
-            //     for (int i = 0; i < rectangleLength; i++)
-            //     {
-            //         length += "*";
-            //     }
-            //     Console.Write(length + "\n");
+                for (int i = 0; i < rectangleLength; i++)
+                {
+                    length += "* ";
+                }
+                Console.Write(length + "\n");
 
-            //     for (int i = 0; i < rectangleWidth - 2; i++)
-            //     {
-            //         for (int u = 1; u <= rectangleLength; u++)
-            //         {
-            //             if ( u == 1 || u == rectangleLength) 
-            //             {
-            //                 Console.Write("*");
-            //             } else {
-            //                 Console.Write("*");
-            //             }
-            //         }
-            //         Console.Write("\n");
-            //     }
-            //     Console.Write(length + "\n");
-            // }
+                for (int i = 0; i < rectangleWidth - 2; i++)
+                {
+                    for (int u = 1; u <= rectangleLength; u++)
+                    {
+                        if ( u == 1 || u == rectangleLength) 
+                        {
+                            Console.Write("* ");
+                        } else {
+                            Console.Write("* ");
+                        }
+                    }
+                    Console.Write("\n");
+                }
+                Console.Write(length + "\n");
+            }
 
-            // {// ex#3.2 прямоугольный 
-            // Console.WriteLine("Enter katet: ");
-            // int katet = int.Parse(Console.ReadLine());
-            // int i = 0;
-            // int j = 0;
-            // while (i < katet)
-            // {
-            //    while (j < katet - i && j >= 0)
-            //    {
-            //        Console.Write("*");
-            //        j++;
-            //    }
-            //   j = 0;
-            //   i++;
-            //   Console.WriteLine();
-            // }
+            {// ex#3.2 прямоугольный 
+            Console.WriteLine("Enter katet: ");
+            int katet = int.Parse(Console.ReadLine());
+            int i = 0;
+            int j = 0;
+            while (i < katet)
+            {
+               while (j < katet - i && j >= 0)
+               {
+                   Console.Write("*");
+                   j++;
+               }
+              j = 0;
+              i++;
+              Console.WriteLine();
+            }
 
-            // }
+            }
             
-            // { // ex #3.3 равносторонний
-            //     Console.WriteLine("Enter side: ");
-            //     int storona = int.Parse(Console.ReadLine());
-            //     int spaceCounter = storona - 1;
-            //     for (int i = 0; i < storona; i++)
-            //     { 
-            //         for (int k = 0; k < spaceCounter; k++) {
-            //             Console.Write(" ");
-            //         }
-            //         spaceCounter--;
-            //         for (int j = 0; j <= i ; j++)
-            //         {
-            //             Console.Write("* ");
-            //         }
-            //         Console.WriteLine();
+            { // ex #3.3 равносторонний
+                Console.WriteLine("Enter side: ");
+                int storona = int.Parse(Console.ReadLine());
+                int spaceCounter = storona - 1;
+                for (int i = 0; i < storona; i++)
+                { 
+                    for (int k = 0; k < spaceCounter; k++) {
+                        Console.Write(" ");
+                    }
+                    spaceCounter--;
+                    for (int j = 0; j <= i ; j++)
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
     
-            //     }
-            // }
+                }
+            }
             { // ex #3.4 ромб
-                int storona = 7;
+                Console.Write("Enter side of romb: ");
+                int storona = int.Parse(Console.ReadLine());
                 int spaceCounter = storona - 1;
                 for (int i = 0; i < storona - 1; i++)
                 { 
@@ -124,10 +125,21 @@ namespace homeWork2803
                     {
                         Console.Write("* ");
                     }
-                    Console.WriteLine();
-    
+                    Console.WriteLine();    
                 }
-                
+            }
+            { // ex #4 проще, чем выводить фигуры ... 
+                Console.Write("Vvedite mesyachny prosent: ");
+                decimal prosent = Convert.ToInt16(Console.ReadLine());
+                decimal nachVklad = 1000m; 
+                int mesyac = 0;
+                while ( nachVklad <= 1100)
+                {
+                    nachVklad = nachVklad + nachVklad * prosent/100;
+                    mesyac++;
+                }
+                Console.WriteLine($"mesyac {mesyac}");
+                Console.WriteLine($"Itogovy vklad {nachVklad}");
             }
         }
     }
